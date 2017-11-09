3303fada4978e118326c322def30c02fd022c68d
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-09 18:17:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,8 @@ function addElementToBeginningOfArray (array, addElement){
   var newArray = [`${addElement}`, ...array];
   return newArray;
 }
+
+function destructivelyAddElementToBeginningOfArray (array, addElement){
+  var array = array.unshift(`${addElement}`);
+  return array;
+}
